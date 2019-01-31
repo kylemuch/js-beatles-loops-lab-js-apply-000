709d1f7ed4cc1146e7a4993209c52b4229adc0ca
Kyle <kylemuch@gmail.com>
2019-01-31 04:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function theBeatlesPlay(musArr, instArr) {
   
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
+// const facts = [
+//   "He was the last Beatle to learn to drive",
+//   "He was never a vegetarian",
+//   "He was a choir boy and boy scout",
+//   "He hated the sound of his own voice"
+// ]
 
 function johnLennonFacts(arr) {
   var index = arr.length - 1
