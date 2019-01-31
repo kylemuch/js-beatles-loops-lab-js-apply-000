80b5694cefbef4d9bea552a8da8cbe5368df33cc
Kyle <kylemuch@gmail.com>
2019-01-31 04:27:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ function theBeatlesPlay(musArr, instArr) {
 }
 
 const facts = [
-  
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
 ]
 
-function johnLennonFacts () {
+function johnLennonFacts (arr) {
+  var index = arr.length - 1
   
+  while (index >= 0) {
+    
+  }
 }
